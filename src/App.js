413cd429fa97ae97d7f230a62b98ceb92a57537c
Bartoszkie <bartek.slysz23@gmail.com>
2020-07-02 14:27:34 +0200

    basic structure for feed-preview

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from "react";
 
-//STYLES
+//Components
+import FeedPreview from "./components/feed-preview/feed-preview.component";
+
+//Pages
+import HomePage from "./pages/home-page/home-page.page";
+
+//Styles
 import "./styles/style.scss";
 
 function App() {
   return (
     <div className="App">
-
+      <HomePage>
+        <FeedPreview url="test" />
+      </HomePage>
     </div>
   );
 }
---
+++ src/components/feed-preview/feed-preview.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FeedPreview = ({ url }) => {
+  return (
+    <div className="feed-preview">
+      <div className="feed-preview__content">
+        <p>Przekazany url</p>
+      </div>
+    </div>
+  );
+};
+
+export default FeedPreview;
